0a593876d737cabe5622ff3ca1df9623afa18425
Ari12sadewa <43287768+Ari12sadewa@users.noreply.github.com>
2026-05-05 19:44:33 +0700

    edit: set etl start date

--- script/DAG.py
+++ script/DAG.py
@@ -33,7 +33,7 @@ CITY_COORDS = {
 # ==============================
 default_args = {
     "owner":        "Ari Sadewa",
-    "start_date":   datetime(2026, 4, 1),
+    "start_date":   datetime(2026, 5, 1),
     "retries":      1,
     "retry_delay":  timedelta(minutes=5),
 }
